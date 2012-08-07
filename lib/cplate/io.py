62862c27e8c9b986e5618b86f4b2f217652929d8
Alexander W Blocker <ablocker@fas.harvard.edu>
2012-08-07 17:43:44 -0400

    Add functions to write numpy arrays to compressed tarball.
    This uses the np.ndarray.tofile interface, which has intelligent blocking to
    avoid integer overflow. It should scale to very large arrays.

--- lib/cplate/io.py
+++ lib/cplate/io.py
@@ -1,6 +1,9 @@
+import tarfile
+import tempfile
+import os, os.path
+
 import numpy as np
 
-# Define functions
 def convert_dtype_to_fmt(dtype, quote=True):
     '''
     Converts dtype from record array to output format
@@ -58,4 +61,51 @@ def write_recarray_to_file(fname, data, header=True, sep=' ', quote=False):
     
     # Close output file
     out_file.close()
+
+def write_arrays_to_tarball(fname, compress='bz2', scratch=None, **kwargs):
+    '''
+    Write arrays (from **kwargs) to tarball at fname with given compression.
+
+    Works be first writing each array to an npy file in a scratch directory,
+    then archiving these files to a (compressed) tarball.
+
+    This handles arrays of sizes that np.savez and np.save with non-file objects
+    cannot handle. It relies upon using pure file objects for np.save that force
+    use of the np.ndarray.tofile function, whcih intelligently chunks binary
+    output to avoid integer overflows.
+    
+    Parameters
+    ----------
+    - fname : string
+        Path for output archive
+    - compress : string
+        Compression to use. Can be 'bz2', 'gz', or ''.
+    - scratch : string
+        Scratch directory for intermediate npy files. Created via
+        tempfile.mkdtemp with default arguments if scratch is None.
+    - **kwargs
+        Arrays to archive.
+
+    Returns
+    -------
+    None
+    '''
+    # Make scratch directory if needed
+    if scratch is None:
+        scratch = tempfile.mkdtemp()
+
+    # Save each array to its own npy file and archive npy files into compressed
+    # tarball
+
+    # Setup tarfile for writing
+    archive = tarfile.open(name=fname, mode='w:' + compress)
+
+    for name, array in kwargs.iteritems():
+        path_npy = os.path.join(scratch, name + '.npy')
+        np.save(path_npy, array)
+        archive.add(path_npy, arcname=name + '.npy')
+        os.remove(path_npy)
+
+    archive.close()
+
 
